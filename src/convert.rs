3fef3e0032f051be32d9146d5ac963cbc6fbe1a9
Brian Anderson <andersrb@gmail.com>
2020-04-20 05:18:41 +0000

    .

--- src/convert.rs
+++ src/convert.rs
@@ -2,7 +2,27 @@ use anyhow::Result;
 use crate::BlogPost;
 use crate::html::SubDom;
 use crate::doc::Document;
+use markup5ever_rcdom as rcdom;
+use rcdom::Handle;
 
 pub fn from_dom(post: &BlogPost, dom: &SubDom) -> Result<Document> {
+    let mut state = State {
+    };
+
+    walk(&mut state, &dom.1);
+
     panic!()
+}
+
+struct State {
+}
+
+fn walk(state: &mut State, node: &Handle) {
+    walk_children(state, node);
+}
+
+fn walk_children(state: &mut State, node: &Handle) {
+    for child in node.children.borrow().iter() {
+        walk(state, &child);
+    }
 }
