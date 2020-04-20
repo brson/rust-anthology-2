2c31abc2ed82376e24e9eecde1d054b5145e6d81
Brian Anderson <andersrb@gmail.com>
2020-04-20 00:53:12 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use serde::Deserialize;
+use log::{info, debug};
+use anyhow::Result;
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt, Debug)]
+struct Opts {
+    #[structopt(subcommand)]
+    command: Command,
+    #[structopt(flatten)]
+    gopts: GlobalOpts,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    DumpConfig,
+}
+
+#[derive(StructOpt, Debug)]
+struct GlobalOpts {
+    #[structopt(default_value = "./data")]
+    data_dir: PathBuf,
+}
+
+#[derive(Deserialize)]
+struct Config {
+}
+
+static CONFIG: &'static str = include_str!("config.toml");
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let opts = Opts::from_args();
+
+    debug!("opts: {:#?}", opts);
+
+    let config = load_config(CONFIG)?;
+
+    Ok(())
+}
+
+fn load_config(s: &str) -> Result<Config> {
+    panic!()
 }
