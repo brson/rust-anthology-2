eeaa3a74c2000ddc0af106ac984c7fe81a3deb51
Brian Anderson <andersrb@gmail.com>
2020-04-20 05:30:10 +0000

    .

--- src/convert.rs
+++ src/convert.rs
@@ -3,10 +3,13 @@ use crate::BlogPost;
 use crate::html::SubDom;
 use crate::doc::Document;
 use markup5ever_rcdom as rcdom;
-use rcdom::Handle;
+use rcdom::{Handle, NodeData};
+use crate::doc;
 
 pub fn from_dom(post: &BlogPost, dom: &SubDom) -> Result<Document> {
     let mut state = State {
+        blocks: Vec::new(),
+        mode: Mode::ScanForBlocks,
     };
 
     walk(&mut state, &dom.1);
@@ -15,10 +18,47 @@ pub fn from_dom(post: &BlogPost, dom: &SubDom) -> Result<Document> {
 }
 
 struct State {
+    blocks: Vec<doc::Block>,
+    mode: Mode,
+}
+
+enum Mode {
+    ScanForBlocks,
+    AccumulateInlines(Vec<doc::Inline>),
 }
 
 fn walk(state: &mut State, node: &Handle) {
+
+    
+
+    match &node.data {
+        NodeData::Element { name, .. } => {
+            match name.local.as_ref() {
+                "p" => {
+                    handle_para(state, node);
+                    return;
+                },
+                _ => {
+                }
+            }
+        }
+        _ => {
+        }
+    }
+
     walk_children(state, node);
+}
+
+fn handle_para(state: &mut State, node: &Handle) {
+    match state.mode {
+        Mode::ScanForBlocks => {
+            state.mode = Mode::AccumulateInlines(Vec::new());
+            walk_children(state, node)
+        },
+        _ => {
+            walk_children(state, node)
+        }
+    }
 }
 
 fn walk_children(state: &mut State, node: &Handle) {
