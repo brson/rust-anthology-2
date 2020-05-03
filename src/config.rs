933efc06655e6f146e9538a8c67f853680b85df7
Brian Anderson <andersrb@gmail.com>
2020-05-03 08:05:14 +0000

    .

--- src/config.rs
+++ src/config.rs
@@ -19,14 +19,6 @@ pub struct ConfigOld {
     pub blog_urls: Vec<Url>,
 }
 
-static BLOG_FILE: &'static str = "./config/blog-posts.toml";
-
-pub fn load_config() -> Result<Config> {
-    let blogs = fs::read_to_string(BLOG_FILE)
-        .context("reading blog file")?;
-    toml::from_str(&blogs)
-        .context("parsing config")
-}
 
 pub fn convert() -> Result<()> {
     let config = load_config_old()?;
@@ -39,18 +31,44 @@ pub fn convert() -> Result<()> {
         }
     });
 
-    let config2 = Config { blog_posts: posts.collect() };
+    let config2 = Config { blog_posts: posts.collect(), authors: vec![] };
 
     let toml = toml::to_string(&config2)?;
 
-    fs::write(BLOG_FILE, &toml)
+    fs::write(BLOG_POSTS_FILE, &toml)
         .context("writing blog-posts-2")?;
 
     Ok(())
 }
 
+static BLOG_POSTS_FILE: &'static str = "./config/blog-posts.toml";
+static AUTHORS_FILE: &'static str = "./config/authors.toml";
+
+pub fn load_config() -> Result<Config> {
+    let blogs = fs::read_to_string(BLOG_POSTS_FILE)
+        .context("reading blog file")?;
+    let blogs: BlogPostsConfig = toml::from_str(&blogs)
+        .context("parsing config")?;
+
+    let authors = fs::read_to_string(AUTHORS_FILE)
+        .context("reading authors file")?;
+    let authors: AuthorsConfig = toml::from_str(&authors)
+        .context("parsing authors")?;
+
+    Ok(Config {
+        blog_posts: blogs.blog_posts,
+        authors: authors.authors,
+    })
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
+    pub blog_posts: Vec<BlogPost>,
+    pub authors: Vec<Author>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BlogPostsConfig {
     pub blog_posts: Vec<BlogPost>,
 }
 
@@ -120,4 +138,17 @@ impl fmt::Display for Category {
         };
         write!(f, "{}", lbl)
     }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AuthorsConfig {
+    authors: Vec<Author>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Author {
+    name: String,
+    github: String,
+    blog: Url,
 }
